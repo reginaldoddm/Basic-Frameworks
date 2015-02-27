907138c3643df4c785ac0fc0766c1ad7db465eea
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-02-27 15:58:15 -0300

    Add arquivos base do sistema

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('SITE_PATH', realpath(dirname(__FILE__)) . DS);
+
+
+//Require core files
+require_once SITE_PATH.'application'.DS.'Request.php';
+require_once SITE_PATH.'application'.DS.'Router.php';
+require_once SITE_PATH.'application'.DS.'BaseController.php';
+require_once SITE_PATH.'application'.DS.'BaseModel.php';
+require_once SITE_PATH.'application'.DS.'Load.php';
+require_once SITE_PATH.'application'.DS.'Registry.php';
+
+
+var_dump(get_included_files());
