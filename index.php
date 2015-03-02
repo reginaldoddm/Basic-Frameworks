cde4e62ef1c453959aa47ed958618a9eb7193673
unknown <reginaldoddm@gmail.com>
2015-03-02 20:29:04 -0300

    Adicionando use

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-use \application\Registry;
+use application\Registry;
+use controllers\indexController;
 
 define('DS', DIRECTORY_SEPARATOR);
 define('SITE_PATH', realpath(dirname(__FILE__)) . DS);
@@ -8,14 +9,12 @@ define('SITE_PATH', realpath(dirname(__FILE__)) . DS);
 
 //Require Autoload
 require_once 'application'.DS.'Autoload.php';
-
-
 require_once 'controllers'.DS.'indexController.php';
 
 $registry = Registry::getInstance();
 $registry->teste = 'Hello World';
 
-$controller = new \controlles\indexController();
+$controller = new indexController();
 
 call_user_func(array($controller, 'index'));
 
