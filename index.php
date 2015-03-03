f7ed728a1d79c4f3e240a4ffd7707306b5974cc2
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-03-03 13:14:51 -0300

    removido linhas de testes.

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-use application\Registry;
 use controllers\indexController;
 
 define('DS', DIRECTORY_SEPARATOR);
@@ -11,11 +10,6 @@ define('SITE_PATH', realpath(dirname(__FILE__)) . DS);
 require_once 'application'.DS.'Autoload.php';
 require_once 'controllers'.DS.'indexController.php';
 
-$registry = Registry::getInstance();
-$registry->teste = 'Hello World';
-
 $controller = new indexController();
 
 call_user_func(array($controller, 'index'));
-
-echo '<pre>'.$registry->myVar.'</pre>';
