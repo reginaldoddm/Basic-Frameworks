897a219ea341f9e54a3490144041b5968216210a
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-03-03 16:23:39 -0300

    Testando class Request

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+use application\Request;
 use controllers\indexController;
 
 define('DS', DIRECTORY_SEPARATOR);
@@ -10,6 +10,5 @@ define('SITE_PATH', realpath(dirname(__FILE__)) . DS);
 require_once 'application'.DS.'Autoload.php';
 require_once 'controllers'.DS.'indexController.php';
 
-$controller = new indexController();
 
-call_user_func(array($controller, 'index'));
+new Request();
