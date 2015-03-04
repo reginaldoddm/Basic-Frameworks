4277e670afc82c1ab656aa17ec41c238acf7cfc4
unknown <reginaldoddm@gmail.com>
2015-03-03 22:44:05 -0300

    resolvendo tabs da IDE

--- application/Request.php
+++ application/Request.php
@@ -1,28 +1,24 @@
 <?php
-
 namespace application;
 
 class Request
 {
-	private $_controller;
-	
-	private $_method;
-	
-	private $_args;
-	
-	
-	public function __construct()
-	{
 
-		$parts = explode('/', $_SERVER['REQUEST_URI']);
-		
-		
-		echo '<pre>';
-		  var_dump($parts);
-		echo '</pre>';
-	}
-	
-	
+    private $_controller;
+
+    private $_method;
+
+    private $_args;
+
+    public function __construct()
+    {
+        $parts = explode('/', $_SERVER['REQUEST_URI']);
+        
+        echo '<pre>';
+        var_dump($parts);
+        echo '</pre>';
+    }
+
     public function getController()
     {
         return $this->_controller;
