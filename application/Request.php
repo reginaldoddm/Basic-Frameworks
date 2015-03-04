b76e7b5e66ee763ea97fdd2321d01b20af5c2d0c
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-03-04 11:13:31 -0300

    Finalizada a classe Request

--- application/Request.php
+++ application/Request.php
@@ -9,25 +9,94 @@ class Request
     private $_method;
 
     private $_args;
+    
+    private $segmets;
 
+    
+    /**
+     * Construtor trata a REQUEST_URI e aloca o controller, method 
+     * e os demais argumentos
+     * 
+     * @access public
+     * @return void
+     */
     public function __construct()
     {
         $parts = explode('/', $_SERVER['REQUEST_URI']);
+        $parts = $this->parts($parts);
         
-        echo '<pre>';
-        var_dump($parts);
-        echo '</pre>';
+        $this->segmets = $parts;
+        
+        $this->_controller = ($c = array_shift($parts)) ? $c : 'index';
+        $this->_method     = ($c = array_shift($parts)) ? $c : 'index';
+        $this->_args       = (isset($parts[0])) ? $parts : array();
+    }
+    
+    /**
+     * Permite que você recupere um segmento específico da ULR
+     * 
+     * @param array $segment
+     * @param mixed $return
+     * @return string
+     */
+    public function getSegment($segment, $return = null)
+    {
+        return  (isset($this->segmets[(int)$segment])) ? : $return;
     }
 
+    /**
+     * Resgata o controller atual
+     */
     public function getController()
     {
         return $this->_controller;
     }
     
-    
+    /**
+     * Resgata o método atual
+     */
     public function getMethod()
     {
         return $this->_method;
+    }
+    
+    
+    /**
+     * Trata as parte da REQUEST_URI
+     * 
+     * @access private
+     * @param array $parts
+     * @return array |boolean
+     */
+    private function parts(array $parts)
+    {
+        $basePath = trim($_SERVER['SCRIPT_NAME'], '/');
+        
+        if (strpos($basePath, '/') !== false) {
+            $basePath = explode('/', $basePath);
+        }
+        
+        if (count($parts) > 0) {
+            $newParts = array();
+            
+            foreach ($parts as $key => $value) {
+                $basePath = (is_array($basePath)) ? $basePath[0] : $basePath;
+                
+                if ($basePath == $value) {
+                    $value = '';
+                }
+                
+                //Remove variaveis vazias
+                if (!empty($value)) {
+                    $newParts[] = $value;
+                }
+            }
+            
+            return $newParts;
+            
+        }
+        return false;
+ 
     }
 }
     
